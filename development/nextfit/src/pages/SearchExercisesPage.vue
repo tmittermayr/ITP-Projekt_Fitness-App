1e07b8397750efb0b04b586f83dabca518f24aa1
tmittermayr <timomittermayr@gmail.com>
2023-01-10 08:53:14 +0100

    add ion-searchbar

--- development/nextfit/src/pages/SearchExercisesPage.vue
+++ development/nextfit/src/pages/SearchExercisesPage.vue
@@ -1,8 +1,11 @@
 <template>
     <Layout>
         <h1 class="text-orange-400">Übungen</h1>
+        <ion-searchbar placeholder="Suchen" color="light"></ion-searchbar>
     </Layout>
 </template>
 <script lang="ts" setup>
 import Layout from '@/components/common/PageLayout.vue';
+import { IonSearchbar } from '@ionic/vue';
+
 </script>
