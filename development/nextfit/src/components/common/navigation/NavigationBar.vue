a39247c39cad9603d4044cdfc8763d9203a55ae4
tmittermayr <timomittermayr@gmail.com>
2023-01-10 22:30:52 +0100

    change bottom navigation bar and add plus container

--- development/nextfit/src/components/common/navigation/NavigationBar.vue
+++ development/nextfit/src/components/common/navigation/NavigationBar.vue
@@ -2,25 +2,28 @@
     <div class="absolute bottom-0 left-0 w-full bg-white py-5 flex justify-around">
         <router-link to="/" class="flex flex-col justify-center gap-2 items-center">
             <ion-icon :icon="home" size="large" class="text-black"></ion-icon>
-            <p class="text-black text-xs">Startseite</p>
         </router-link>
         <router-link to="/workouts" class="flex flex-col justify-center gap-2 items-center">
             <ion-icon :icon="barbell" size="large" class="text-black"></ion-icon>
-            <p class="text-black text-xs">Training</p>
         </router-link>
+        <div class="flex justify-center items-center w-16">
+            <ion-fab>
+                <ion-fab-button color="primary" class="w-20 h-20">
+                    <ion-icon :icon="add" size="large"></ion-icon>
+                </ion-fab-button>
+            </ion-fab>
+        </div>
         <router-link to="/exercises" class="flex flex-col justify-center gap-2 items-center">
             <ion-icon :icon="search" size="large" class="text-black"></ion-icon>
-            <p class="text-black text-xs">Übungen</p>
         </router-link>
         <router-link to="/settings" class="flex flex-col justify-center gap-2 items-center">
             <ion-icon :icon="settings" size="large" class="text-black"></ion-icon>
-            <p class="text-black text-xs">Einstellungen</p>
         </router-link>
     </div>
 </template>
   
 <script lang="ts" setup>
-import { home, barbell, search, settings } from "ionicons/icons"
+import { home, barbell, search, settings, add } from "ionicons/icons"
 import { IonIcon } from "@ionic/vue"
 
 </script>
