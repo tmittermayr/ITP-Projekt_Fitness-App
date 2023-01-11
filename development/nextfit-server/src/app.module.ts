bb9431e0d865c2e5d530f387e62ca5c341f99e0c
Valentin Seeger <vseeger@MacBook-Pro-von-Valentin.local>
2023-01-11 21:52:53 +0100

    Created database connection and CRUD for exercise endpoint

--- development/nextfit-server/src/app.module.ts
+++ development/nextfit-server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExerciseModule } from './exercise/exercise.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admindb:EJ82GMbhXvA1YTq7@nextfitdb.qfweniu.mongodb.net/?retryWrites=true&w=majority'
+    ),
+    ExerciseModule],
   controllers: [AppController],
   providers: [AppService],
 })
