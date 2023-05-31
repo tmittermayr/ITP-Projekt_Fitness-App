62f237f2556d9b6b60df0a3dc5ed6530d30c1238
tmittermayr <timomittermayr@gmail.com>
2023-05-31 17:23:45 +0200

    add functionality to add sets

--- development/nextfit/src/components/exercises/TrainingsOverview.vue
+++ development/nextfit/src/components/exercises/TrainingsOverview.vue
@@ -2,11 +2,39 @@
     <div class="px-3 mt-5 text-center" v-if="!addExerciseListOpened">
         <div class="mb-10">
             <h2 class="font-semibold">{{ training.title }}</h2>
-            <p class="text-red-500">{{ timer }}</p>
         </div>
         <div class="flex flex-col items-center">
             <div>
-                
+                <div v-for="(exercise, index) in exercises" :key="index" class="mb-10">
+                    <h2>{{ exercise.id }}</h2>
+                    <div v-for="(set, index) in exercise?.sets" :key="index" class="flex gap-5 items-center justify-between my-2">
+                        <h3 class="m-0">{{ index + 1 }}</h3>
+                        <input type="text" disabled class="bg-gray-100 rounded border-gray-200 border-2 w-10 h-8 text-lg text-center" placeholder="10" :value="set.reps" >
+                        <div class="flex gap-1">
+                            <input type="text" disabled class="bg-gray-100 rounded border-gray-200 border-2 w-14 h-8 text-lg text-center" placeholder="80" :value="set.weight" >
+                            <p class="text-lg">kg</p>
+                        </div>
+                        <select name="type" class="bg-gray-100 rounded border-gray-200 border-2" >
+                            <option value="warmup">Aufwärmen</option>
+                            <option value="superset">Superset</option>
+                        </select>
+                    </div>
+                    <div class="flex gap-5 items-center justify-between my-2">
+                        <h3 class="m-0">{{ exercise?.sets.length + 1 }}</h3>
+                        <input type="text" class="bg-gray-100 rounded border-gray-200 border-2 w-10 h-8 text-lg text-center" placeholder="10" v-model="currentReps" >
+                        <div class="flex gap-1">
+                            <input type="text" class="bg-gray-100 rounded border-gray-200 border-2 w-14 h-8 text-lg text-center" placeholder="80" v-model="currentWeight"  >
+                            <p class="text-lg">kg</p>
+                        </div>
+                        <select name="type" class="bg-gray-100 rounded border-gray-200 border-2" >
+                            <option value="warmup">Aufwärmen</option>
+                            <option value="superset">Superset</option>
+                        </select>
+                    </div>
+                    <div class="flex justify-end">
+                        <Button @click="addSet(exercise?.id)" v-show="isValid">Speichern</Button>
+                    </div>
+                </div>
             </div>
             <Button class="w-full" @click="addExerciseListOpened = true">Übung auswählen</Button>
         </div>
@@ -15,51 +43,53 @@
 </template>
 <script lang="ts" setup>
 import store from '@/store/store';
-import { ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import Button from '../common/ButtonComponent.vue';
 import AddExerciseList from './AddExerciseList.vue';
 
-
 const training = store.state.trainingsInformation
-const timer = ref('...')
 const addExerciseListOpened = ref(false)
 
-window.setInterval(function(){
-    // get total seconds between the times
-    let delta = Math.abs((new Date(training.startdatetime)).getSeconds() - new Date().getSeconds());
-    
-    // calculate (and subtract) whole hours
-    const hours = Math.floor(delta / 3600) % 24;
-    delta -= hours * 3600;
-    
+const exercises = ref([])
 
-    // calculate (and subtract) whole minutes
-    const minutes = Math.floor(delta / 60) % 60;
-    delta -= minutes * 60;
-
-    // what's left is seconds
-    const seconds = delta;  // in theory the modulus is not required
-    if(hours < 10) {
-        if(minutes < 10) {
-            if(seconds < 10) {
-                timer.value = '0' + hours + ':0' + minutes + ":0" + seconds
-            } else {
-                timer.value = '0' + hours + ':0' + minutes + ":" + seconds
-            }
-        } else {
-            if(seconds < 10) {
-                timer.value = '0' + hours + ':' + minutes + ":0" + seconds
-            } else {
-                timer.value = '0' + hours + ':' + minutes + ":" + seconds
-            }
-            
-        }
-    } else if(seconds < 10) {
-        timer.value = hours + ':0' + minutes + ":" + seconds
-    } else {
-        timer.value = hours + ':' + minutes + ":" + seconds
+onMounted(() => {
+    for(let i = 0; i < training.exerciseids.length; i++) {
+        exercises.value.push({
+            id: training.exerciseids[i].exerciseid,
+            sets: [],
+        })
     }
-}, 500);
+    console.log(exercises.value);
+})
+
+function saveSets() {
+    console.log("a");
+}
+
+const currentReps = ref('')
+const currentWeight = ref('')
+
+const isValid = computed(() => {
+    if(currentReps.value != '' && currentWeight.value != '') {
+        return true
+    }
+    return false
+})
+
+const isSetOpen = ref(true)
+
+function addSet(id: string) {
+    console.log(currentWeight.value);
+    
+    for(let i = 0; i < exercises.value.length; i++) {
+        if(training.exerciseids[i].exerciseid == id) {
+            exercises.value[i].sets.push({ weight: currentWeight.value, reps: currentReps.value })
+        }
+    }
+    currentReps.value = ''
+    currentWeight.value = ''
+    
+}
 
 
 </script>
--- development/nextfit/src/pages/SettingsPage.vue
+++ development/nextfit/src/pages/SettingsPage.vue
@@ -2,6 +2,9 @@
     <Layout>
         <div class="p-5 bg-gray-100 w-full h-full absolute top-0 left-0 px-5">
             <page-header title="Einstellungen"></page-header>
+            <div>
+                <InputComponent :model="getToken()" />
+            </div>
             <router-link to="login" class="text-orange-400 text-center">Abmelden</router-link>
         </div>
     </Layout>
@@ -10,4 +13,15 @@
 import Layout from '@/components/common/PageLayout.vue';
 import PageHeader from '@/components/common/PageHeader.vue';
 import { RouterLink } from 'vue-router';
+import InputComponent from '@/components/common/InputComponent.vue';
+import { ref } from 'vue';
+import { Preferences } from '@capacitor/preferences';
+
+const token = ref()
+
+async function getToken() {
+    const { value } = await Preferences.get({ key: 'token' });
+    
+    token.value = value;
+}
 </script>
