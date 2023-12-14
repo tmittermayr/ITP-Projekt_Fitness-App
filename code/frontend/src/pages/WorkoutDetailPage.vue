6fd4b07554c8ec570b8c7fe55475e021bc48351c
tmittermayr <71379141+tmittermayr@users.noreply.github.com>
2023-12-14 19:04:26 +0100

    add option to convert a normal training into a plan

--- code/frontend/src/pages/WorkoutDetailPage.vue
+++ code/frontend/src/pages/WorkoutDetailPage.vue
@@ -5,6 +5,11 @@
         </ion-button>
         <div class="pt-14 flex px-10 flex-col gap-5 bg-gray-100 min-h-screen">
             <h2 class="font-semibold text-3xl text-center text-orange-400">{{ workout?.title }}</h2>
+            <div class="flex justify-center">
+                <button-component v-if="!workout?.isTrainingsPlan" @click="openPlanAlert()">Trainingsplan erstellen</button-component>
+                <p class="text-green-500 text-lg" v-else>Ist bereits ein Trainingsplan.</p>
+            </div>
+         
             <div class="flex flex-col items-center">
                 <div>
                     <div v-for="(exercise, index) in workout?.exerciseids" :key="index" class="mb-10">
@@ -35,9 +40,41 @@ import axios from 'axios';
 import { onMounted, ref } from 'vue';
 import { useRoute } from "vue-router";
 import Layout from '@/components/common/PageLayout.vue';
-import { chevronBackOutline } from 'ionicons/icons';
-import { IonIcon } from '@ionic/vue';
-import { IonButton } from '@ionic/vue';
+import { chevronBackOutline, create } from 'ionicons/icons';
+import { IonIcon, alertController } from '@ionic/vue';
+import ButtonComponent from '@/components/common/ButtonComponent.vue';
+import router from "@/router";
+import { IonAlert, IonButton } from '@ionic/vue';
+
+async function openPlanAlert(){
+            const alert = await alertController.create({
+                header: 'Name für deinen Trainingsplan:',
+                buttons: [
+                    {
+                        text: 'Abbrechen',
+                        role: 'cancel'
+                    },
+                    {
+                        text: 'Speichern',
+                        role: 'confirm',
+                    }
+                ],
+                inputs: [
+                    {
+                        placeholder: 'Name',
+                    }
+                ]
+            });
+            
+            await alert.present();
+            
+            const result = await alert.onDidDismiss();
+            
+            if(result.role == 'confirm') {
+                createTrainingsplan(workout.value._id, result.data.values[0])
+            }
+            
+        }
 
 const route = useRoute()
 const token = ref()
@@ -70,6 +107,21 @@ async function assignNames() {
     for(const exercise of workout.value.exerciseids) {
         exercise.name = await getName(exercise.exerciseid)
     }
+}
+
+async function createTrainingsplan(id: string, name: string) {    
+    console.log(id, name);
+    
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token.value}`;
+    await axios.patch('http://localhost:3000/training/toTrainingsPlan/' + id, {
+        title: name == '' ? workout.value.title : name
+    })
+    .then(async function (response) {
+        router.push('/workouts')
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 }
 
 const training = new TrainingsInformation()
