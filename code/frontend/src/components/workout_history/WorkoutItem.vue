c563838fb9cf84240b28047ab655abc385f6cb60
tmittermayr <71379141+tmittermayr@users.noreply.github.com>
2023-12-14 19:02:35 +0100

    update props of workoutitem.vue

--- code/frontend/src/components/workout_history/WorkoutItem.vue
+++ code/frontend/src/components/workout_history/WorkoutItem.vue
@@ -1,6 +1,11 @@
 <template>
     <router-link
-        :to="{
+        :to="props.isPlan ? {
+            name: 'TrainingsplanDetail',
+            params: {
+                id: workout._id
+            }
+        } : {
             name: 'WorkoutDetail',
             params: {
                 id: workout._id
@@ -9,7 +14,7 @@
     <div class="bg-white p-5 rounded-lg flex justify-between items-center shadow-lg text-black">
         <div>
             <h2 class="text-xl mb-1 mt-0">{{ props.workout?.title }}</h2>
-            <p>Ganzkörper</p>
+            <p>{{ formatDate(props.workout.startdatetime) }}</p>
         </div>
         <ion-icon :icon="chevronForwardOutline" size="large" />
     </div>
@@ -20,7 +25,26 @@
 import { IonIcon } from '@ionic/vue';
 import { chevronForwardOutline } from 'ionicons/icons';
 
+
 const props = defineProps({
     workout: Object,
+    isPlan: Boolean,
 })
+
+function formatDate(date: string) {
+    const d = new Date(date);
+    const yyyy = d.getFullYear();
+    let mm = d.getMonth() + 1;
+    let dd = d.getDate();
+    let hh = d.getHours();
+    let min = d.getMinutes();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    if (hh < 10) hh = '0' + hh;
+    if (min < 10) min = '0' + min;
+
+    return dd + '.' + mm + '.' + yyyy + ' ' + hh + ':' + min;
+}
+
 </script>
